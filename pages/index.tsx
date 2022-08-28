299b6e09c645eb4ed26cdb169d2e05e67781050d
Ahim11 <ahim.mudbari@gmail.com>
2022-08-28 22:35:41 +0545

    index fild with many components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import HeadlessUi from '../components/HeadlessUi';
 import Landing from '../components/Landing';
 import styles from '../styles/Home.module.css';
 
@@ -14,9 +15,15 @@ export default function Home() {
       </Head>
 
       <Header />
-      <main>
+      <main className='relative h-[200vh]' >
         <Landing />
       </main>
+      <section className='relative z-40 bg-[#011121] -mt-[100vh] min-h-screen '>
+        <div className='space-y-10 py-14'>
+          <h1 className='text-4xl font-medium tracking-wide text-center text-gray-300'>Products</h1>
+          <HeadlessUi />
+        </div>
+      </section>
     </div>
   );
 }
