8cfda93591d58aa4236f1eacae53d766cb3e7868
Ahim11 <ahim.mudbari@gmail.com>
2022-08-31 22:21:40 +0545

    adding categories and product

--- components/HeadlessUi.tsx
+++ components/HeadlessUi.tsx
@@ -1,33 +1,34 @@
 import { Tab } from '@headlessui/react'
-import { GetServerSideProps } from 'next'
 import React from 'react'
-import { fetchCategories } from '../utils/fetchCategories'
+import Product from './Product';
+interface Props {
+    categories: [Category];
+    products: [Products]
+}
+const HeadlessUi = ({ categories, products }: Props) => {
 
-const HeadlessUi = ({ categories }) => {
-    console.log(categories);
-
+    const showProducts = (category: number) => {
+        return products.filter((product) => product.category._ref === categories[category]._id).map((products) => (
+            <Product key={products._id} product={products} />
+        ))
+    }
     return (
         <Tab.Group>
-            <Tab.List className='flex justify-center space-x-5'>
-                <Tab>good</Tab>
-                <Tab>bad</Tab>
-                {/* {categories.map((category) => (
+            <Tab.List className='flex justify-center space-x-4'>
+
+                {categories.map((category) => (
                     <Tab
                         key={category._id}
                         id={category._id}
-                        className={({ slelected }) => {
-                            `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${slelected ? "borderGradient bg-[#353B3C] text-white" : "border-b-2 border-[#353B3c] text-red-400"}`
-                        }}
+                        className={({ selected }) => `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${selected ? "borderGradient bg-[#353B3C] text-white" : "border-b-2 border-[#353B3c] text-red-400"}`}
                     >{category.title}</Tab>
-                ))} */}
+                ))}
             </Tab.List>
             <Tab.Panels className="pt-10 pb-24 mx-auto max-w-fit sm:px-4">
-                {/* <Tab.Panel className="tabPanel">{showProduct(0)}</Tab.Panel>
-                <Tab.Panel  className="tabPanel">{showProduct(1)}</Tab.Panel>
-                <Tab.Panel  className="tabPanel">{showProduct(2)}</Tab.Panel>
-                <Tab.Panel  className="tabPanel">{showProduct(3)}</Tab.Panel> */}
-                <Tab.Panel>I am good boy</Tab.Panel>
-                <Tab.Panel>I am bad boy</Tab.Panel>
+                <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+                <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+                <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+                <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
 
             </Tab.Panels>
         </Tab.Group>
