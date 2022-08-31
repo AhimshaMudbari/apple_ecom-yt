b6026db9e2a67cbd209a774a1cd5b420d7d50a77
Ahim11 <ahim.mudbari@gmail.com>
2022-08-31 22:24:15 +0545

    configuring interface for category and product

--- typings.d.ts
+++ typings.d.ts
@@ -10,3 +10,30 @@ interface Category{
     };
     title:string;
 }
+
+interface Image{
+    _key:string;
+    _type:"image";
+    asset:{
+        url:string;
+    }
+}
+interface Products{
+    _id:string;
+    _createdAt: string;
+    _updatedAt: string;
+    _rev:string;
+    _type: "product";
+    slug:{
+        _type:"slug";
+        current:"string";
+    };
+    title:string;
+    priice:number;
+    description:string;
+    category:{
+        _type: "reference";
+        _ref: string;
+    };
+    image: [image];
+}
